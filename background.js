georgyangelov/Dark-youtube-plugin-chrome

commit 31d8d2ce17c7909b10610f8415a5a40597b5e61e

Fix white background for comments.
Add a content_script to
http://apis.google.com/u/0/wm/4/_/widget/render/comments*.

background.js
@@ -6,21 +6,27 @@ if (localStorage.activated == undefined)
 var portsByTabId = {};
 
 chrome.extension.onConnect.addListener(function(port) {
-	portsByTabId[port.sender.id] = port;
+	if (!portsByTabId[port.sender.id])
+		portsByTabId[port.sender.id] = new Array();
+	portsByTabId[port.sender.id].push(port);	
 });
 
 function sendMessage(message)
 {
 	for (var tabid in portsByTabId)
 	{
-		var port = portsByTabId[tabid];
-		try
+		var ports = portsByTabId[tabid];
+		for (var i = 0; i < ports.length; i++) 
 		{
-			port.postMessage(message);
-		}
-		catch (e)
-		{
-			delete portsByTabId[tabid];
+			try
+			{
+				ports[i].postMessage(message);
+			}
+			catch (e)
+			{
+				/* Assume that if one port from a tab is missing, the rest are missing too. */
+				delete portsByTabId[tabid];
+			}
 		}
 	}
 }

youtube_comments.js
@@ -1,0 +1,44 @@
+var active = false;
+var head, link;
+
+function activate()
+{
+	head  = document.getElementsByTagName('head')[0];
+    link = document.createElement('link');
+    link.id   = "darkstyle_css";
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = chrome.extension.getURL('dark_comments.css');
+    link.media = 'all';
+    head.appendChild(link);
+
+    active = true;
+}
+
+chrome.extension.sendMessage({method: "isActivated"}, function(response) {
+  if (response.status == "true")
+  {
+	  	activate();
+  }
+});
+
+var myPort = chrome.extension.connect();
+
+myPort.onMessage.addListener(function(data) {
+    if ( data == "true" )
+    {
+        if (!active)
+        {
+        	activate();
+        }
+    }
+    else
+    {
+    	if (active)
+    	{
+        	head.removeChild(link);
+
+        	active = false;
+    	}
+   	}
+});

dark_comments.css
@@ -1,0 +1,48 @@
+body,
+{
+	color: #fff !important;
+	background: #1b1b1b !important;
+}
+
+.kv8eNc, .yJa, .Mza, .DJa, .uPa
+{
+	background-color: #1b1b1b !important;
+}
+
+.DJa
+{
+	background: none !important;
+	color: #fff !important;
+}
+
+.ve.oba, .dn
+{
+	color: #666 !important;
+}
+
+/* Commenter name */
+.Ub
+{
+	color: #ccc !important;
+}
+
+/* Show More Button */
+.d-s 
+{
+	background-color: #454545 !important;
+	border-color: #333 #333 #000 !important;
+	text-shadow: 0 1px 0 rgba(0, 0, 0, .45) !important;
+	outline: 0 !important;
+	background-image: linear-gradient(to bottom,#474747 0,#2B2B2B 100%) !important;
+	background-image: -moz-linear-gradient(top,#474747 0,#2B2B2B 100%) !important;
+	background-image: -webkit-linear-gradient(top,#474747 0,#2B2B2B 100%) !important;
+	background-image: -o-linear-gradient(top,#474747 0,#2B2B2B 100%) !important;
+	background-image: -ms-linear-gradient(top,#474747 0,#2B2B2B 100%) !important;
+	filter: progid:DXImageTransform.Microsoft.Gradient(GradientType=0, startColorStr="#FF474747", endColorStr="#FF2B2B2B") !important;
+	box-shadow: inset 0 1px 0 rgba(31, 31, 31, .45)) !important;
+	-moz-box-shadow: inset 0 1px 0 rgba(31,31,31,.45) !important;
+	-webkit-box-shadow: inset 0 1px 0 rgba(31,31,31,.45) !important;
+	-ms-box-shadow: inset 0 1px 0 rgba(31,31,31,.45) !important;
+	color: #fff !important;
+}
+

commit c2186b72f3daff9fc077573f9d57c049d2bdfcd8

Fix extension enable/disable toggle.
Fix a regression introduced in 31d8d2ce17c7909b10610f8415a5a40597b5e61e.

background.js
@@ -3,32 +3,26 @@ if (localStorage.activated == undefined)
 	setIcon(true);
 }
 
-var portsByTabId = {};
+var portsByScriptId = {};
 
-chrome.extension.onConnect.addListener(function(port) {
-	if (!portsByTabId[port.sender.id])
-		portsByTabId[port.sender.id] = new Array();
-	portsByTabId[port.sender.id].push(port);	
+chrome.runtime.onConnect.addListener(function(port) {
+    portsByScriptId[port.sender.id + port.name] = port;
 });
 
 function sendMessage(message)
 {
-	for (var tabid in portsByTabId)
-	{
-		var ports = portsByTabId[tabid];
-		for (var i = 0; i < ports.length; i++) 
-		{
-			try
-			{
-				ports[i].postMessage(message);
-			}
-			catch (e)
-			{
-				/* Assume that if one port from a tab is missing, the rest are missing too. */
-				delete portsByTabId[tabid];
-			}
-		}
-	}
+        for (var contentScriptId in portsByScriptId)
+        {
+                var port = portsByScriptId[contentScriptId];
+                try
+                {
+                        port.postMessage(message);
+                }
+                catch (e)
+                {
+                        delete portsByScriptId[contentScriptId];
+                }
+        }
 }
 
 function updateIcon()
@@ -62,7 +56,7 @@ function setIcon(dark)
 	sendMessage(localStorage.activated);
 }
 
-chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.method == "isActivated")
       sendResponse({status: localStorage.activated});
     else

youtube.js
@@ -15,14 +15,14 @@ function activate()
     active = true;
 }
 
-chrome.extension.sendMessage({method: "isActivated"}, function(response) {
+chrome.runtime.sendMessage({method: "isActivated"}, function(response) {
   if (response.status == "true")
   {
 	  	activate();
   }
 });
 
-var myPort = chrome.extension.connect();
+var myPort = chrome.runtime.connect({name: "main"});
 
 myPort.onMessage.addListener(function(data) {
     if ( data == "true" )

youtube_comments.js
@@ -15,14 +15,14 @@ function activate()
     active = true;
 }
 
-chrome.extension.sendMessage({method: "isActivated"}, function(response) {
+chrome.runtime.sendMessage({method: "isActivated"}, function(response) {
   if (response.status == "true")
   {
 	  	activate();
   }
 });
 
-var myPort = chrome.extension.connect();
+var myPort = chrome.runtime.connect({name: "comments"});
 
 myPort.onMessage.addListener(function(data) {
     if ( data == "true" )